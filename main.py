e0275ff80d65d71dcfde48cc26ee3208294d7063
DimaD1989 <d_d1989sv@mail.ru>
2023-06-22 19:40:29 +0300

    task 3

--- main.py
+++ main.py
@@ -30,3 +30,10 @@
 #     return {name: sale / 100 * bon for name, sale, bon in zip(names, salary, (float(i[:-1]) for i in bonus))}.items()
 # print(salary_all(names,salary,bonus))
 
+# Создайте функцию генератор чисел Фибоначчи(см.Википедию).
+def fib(n):
+    a, b = 0, 1
+    for __ in range(n):
+        yield a
+        a, b = b, a + b
+print(list(fib(20)))
