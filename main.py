33d95c80d71145e4f1f63a580537f51b88439a66
DimaD1989 <d_d1989sv@mail.ru>
2023-06-22 19:17:12 +0300

    task 1

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # ✔ Напишите функцию, которая принимает на вход строку —
 # абсолютный путь до файла. Функция возвращает кортеж из трёх
 # элементов: путь, имя файла, расширение файла.
+
+my_path = r'C:\Users\d_d19\OneDrive\Рабочий стол\Python\python.txt'
+
+def separate_path(my_path:str) -> tuple():
+    list_my_path = my_path.split('\\')
+    list_last_elem = list_my_path[-1].split('.')
+    path = '\\'.join(list_my_path[0:-1])
+    name = list_last_elem[0]
+    expansion = list_last_elem[-1]
+    return path, name, expansion
+
+print(separate_path(my_path))
+
